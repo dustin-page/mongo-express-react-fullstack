7e45448ae52b3f8a6743a96e8e9b522938ce92f2
Dustin Page <dustinpage@live.com>
2019-08-14 12:33:05 -0500

    Initial Commit - Set up webpack, babel scaffolding for application

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname,'src','app'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?/, //test determines if a file should be compiled. the regex processes js and jsx files; ? mark makes the x optional
+            loader: 'babel-loader'
+        }]
+    }
+}
